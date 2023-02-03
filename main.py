214228c701387b4570697fcdcf351109af2bd320
Nolwenn-Borgne <nolwenn.borgne@gmail.com>
2023-02-03 12:06:59 +0100

    add Procfile

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # 1. Library imports
-import uvicorn ##ASGI
+#import uvicorn ##ASGI
 from fastapi import FastAPI
 from Tweets import Tweet
 from keras.models import load_model 
@@ -121,6 +121,6 @@ def predict(data:Tweet):
 
 # 8. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+#if __name__ == '__main__':
+#    uvicorn.run(app, host='0.0.0.0', port=8000)
 #uvicorn main:app --reload
